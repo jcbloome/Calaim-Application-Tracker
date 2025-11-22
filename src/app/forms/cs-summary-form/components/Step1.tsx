75034de4d54303ce09b3fdfe3556250489281124
Jason Bloome <jason@carehomefinders.com>
2025-11-22 06:05:27 +0000

    fatal error not being resolved and freezing the app

--- src/app/forms/cs-summary-form/components/Step1.tsx
+++ src/app/forms/cs-summary-form/components/Step1.tsx
@@ -18,10 +18,9 @@ export default function Step1() {
   const dob = watch('memberDob');
 
   useEffect(() => {
-    const dobValue = watch('memberDob');
-    if (dobValue) {
+    if (dob) {
       const today = new Date();
-      const birthDate = new Date(dobValue);
+      const birthDate = new Date(dob);
       let age = today.getFullYear() - birthDate.getFullYear();
       const m = today.getMonth() - birthDate.getMonth();
       if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
