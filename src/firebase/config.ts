d94bbc8e4c58bcf5e855c02273a0d63aadd47172
Jason Bloome <jason@carehomefinders.com>
2025-11-22 05:10:15 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4549710902-46566",
+  "appId": "1:994959150214:web:123aec84ef26c1e4415507",
+  "apiKey": "AIzaSyCSmsCXIPmNOJWKUHXpKJYGUNs2ed2lOF8",
+  "authDomain": "studio-4549710902-46566.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "994959150214"
+};
