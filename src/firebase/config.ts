4247ab062f6970d19e42008269b19b1d99d6f2e9
Jason Bloome <jason@carehomefinders.com>
2025-12-07 13:40:37 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,5 @@ export const firebaseConfig = {
   "appId": "1:994959150214:web:123aec84ef26c1e4415507",
   "apiKey": "AIzaSyCSmsCXIPmNOJWKUHXpKJYGUNs2ed2lOF8",
   "authDomain": "studio-4549710902-46566.firebaseapp.com",
-  "measurementId": "",
   "messagingSenderId": "994959150214"
 };
