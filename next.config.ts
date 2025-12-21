c7052da5dd20595b70c9c16560c63e8b3ffa734d
Jason Bloome <jason@carehomefinders.com>
2025-12-21 15:55:11 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    watcherOptions: {
-      ignored: ['**/.genkit/**'],
-    },
-  },
   images: {
     remotePatterns: [
       {
