4523a9b84f3fd1806e2f1c9e94d8bf33baa3421c
Jason Bloome <jason@carehomefinders.com>
2025-12-23 17:25:09 +0000

    on the faq page what do i need to do should n't be centerted

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -29,7 +29,7 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center justify-between p-4 font-medium transition-all hover:bg-muted/50 rounded-t-lg data-[state=open]:bg-muted/50 data-[state=open]:rounded-b-none [&[data-state=open]>svg]:rotate-180",
+        "flex flex-1 items-center justify-between p-4 font-medium text-left transition-all hover:bg-muted/50 rounded-t-lg data-[state=open]:bg-muted/50 data-[state=open]:rounded-b-none [&[data-state=open]>svg]:rotate-180",
         className
       )}
       {...props}
