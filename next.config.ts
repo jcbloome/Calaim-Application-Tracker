ae6d7be8f8dde481842970941c4e65f5b2d00585
Jason Bloome <jason@carehomefinders.com>
2025-12-30 16:03:28 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -23,11 +23,12 @@ const nextConfig: NextConfig = {
     ]
   },
   experimental: {
-    // This is the correct place for allowedDevOrigins in Next.js 14+
-    allowedDevOrigins: [
-        '6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev'
-    ]
-  }
+    // Other experimental features can be added here.
+  },
+  // This is the correct location for allowedDevOrigins
+  allowedDevOrigins: [
+      '6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev'
+  ]
 };
 
 export default nextConfig;
