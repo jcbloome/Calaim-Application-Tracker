ba10879f2b9a0e5279df6f44787149b82af18080
Jason Bloome <jason@carehomefinders.com>
2026-01-05 05:42:12 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /**
-   * This allows the Next.js dev server to accept
-   * requests/pop-ups originating from Firebase Studio.
-   */
-  experimental: {
-     allowedDevOrigins: [
-        '*.firebase.studio',
-        '6000-firebase-studio-1763747953373.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev'
-    ]
-  },
   images: {
     remotePatterns: [
       {
