423bc5619533de9ba09cf5cf2ae52d215d7d5bc2
Jason Bloome <jason@carehomefinders.com>
2026-01-08 06:32:13 +0000

    When enabled, log entries appear in reverse chronological order. When en

--- src/app/admin/applications/page.tsx
+++ src/app/admin/applications/page.tsx
@@ -20,6 +20,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { useAdmin } from '@/hooks/use-admin';
 import { FirestorePermissionError } from '@/firebase/errors';
